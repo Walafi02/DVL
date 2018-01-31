453ef0b18f6eb3fe192f242aaaf39b347bae9132
Walafi02 <walafif@yahoo.com>
2018-01-31 20:37:18 -0300

    Script em Python para adicionar usuarios

---
+++ arquivos/addUserProf.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import os
+import MySQLdb
+import getpass
+
+class Banco:
+	def __init__(self):
+		self.host = "localhost"
+		self.user = "root"
+		self.passwd = "root"
+		self.bd = "cadastro"
+
+		self.conexao = MySQLdb.connect(self.host, self.user, self.passwd, self.bd)
+		self.cursor = self.conexao.cursor()
+
+
+	def verifica(self, login):
+		self.cursor.execute("SELECT * FROM usuarios WHERE login = '%s'" % (login))
+		return len(self.cursor.fetchall())
+
+	def add(self, login, senha):
+		self.cursor.execute("INSERT INTO usuarios (tipo, login, senha) VALUES ('comum', '%s', '%s');" % (login, senha))
+		self.conexao.commit()
+
+	def close(self):
+		self.conexao.close()
+
+
+banco = Banco()
+
+while True:
+	os.system("clear")
+	login = raw_input("Login: ")
+	senha = getpass.getpass('Senha: ')
+	repeteSenha = getpass.getpass('Repita a Senha: ')
+
+	if banco.verifica(login) > 0:
+		os.system("clear")
+		raw_input("Login ja existente!")
+
+	elif senha != repeteSenha:
+		os.system("clear")
+		raw_input("Senhas Nao Sao idanticas!")
+	
+	elif len(senha) < 8:
+		os.system("clear")
+		raw_input("Senha Muito Curta!")
+	
+	else:
+		banco.add(login, senha)
+		os.system("clear")
+		raw_input("Processo Bem Secedido!")
+		break
+
+banco.close()
