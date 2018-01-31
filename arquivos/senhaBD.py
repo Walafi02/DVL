45c19f25e1900b9fae7b26376f475b42988859c2
Walafi02 <walafif@yahoo.com>
2018-01-31 20:38:04 -0300

    Script em Python que altera as senhas nos arquivos do projeto

---
+++ arquivos/senhaBD.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import os
+import MySQLdb
+import getpass
+
+while True:
+ 	os.system("clear")
+ 	passwd = getpass.getpass('Senha: ')
+
+ 	try:
+ 		conexao = MySQLdb.connect("localhost", "root", passwd, "cadastro")
+ 		conexao.close()
+
+		os.system("sed -ie \"s/\$pass =.*/\$pass = \\\"%s\\\";/g\" ../busca_banco/conexao.php" % (passwd))
+		os.system("sed -ie \"s/self.passwd =.*/self.passwd = \\\"%s\\\"/g\" addUserProf.py" % (passwd))
+
+ 		os.system("clear")
+ 		print "Senha Atualizado nos Arquivos"
+ 		break
+ 	except:
+ 		os.system("clear")
+ 		raw_input("Senha Errada!")
