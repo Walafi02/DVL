1005ff70d476d9a1513b82253cd6b86a5e8e73c5
Walafi02 <walafif@yahoo.com>
2018-01-31 14:25:43 -0300

    Update confFinais.sh

--- arquivos/confFinais.sh
+++ arquivos/confFinais.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo echo "www-data ALL=NOPASSWD:   ALL" #>> /etc/sudors
+sudo echo "www-data ALL=NOPASSWD:   ALL" >> /etc/sudoers
 
 sudo echo "
 %alunos ALL=NOPASSWD: \
-       	/usr/bin/docker exec -it * login" #>> /etc/sudors
+       	/usr/bin/docker exec -it * login" >> /etc/sudoers
+
+cd ../users
+sudo chmod 764 add.sh
 
 cd ../..
 sudo chown www-data.www-data -R DVL
-
-cd DVL/users
-sudo chmod 764 add.sh
