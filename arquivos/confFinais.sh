425d1ab3693ac598b31d548c63fcfd1d9aaa7e5c
Walafi02 <walafif@yahoo.com>
2018-02-26 14:57:10 -0300

    correcao de comandos

--- arquivos/confFinais.sh
+++ arquivos/confFinais.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-sudo echo "www-data ALL=NOPASSWD:   ALL" >> /etc/sudoers
+sudo echo "www-data        ALL=NOPASSWD:\\
+                        /usr/bin/docker,\\
+                        /usr/sbin/addgroup,\\
+                        /usr/sbin/useradd,\\
+                        /bin/echo *,\\
+                        /usr/sbin/chpasswd,\\
+                        /usr/bin/passwd,\\
+                        /bin/rm senhas.txt,\\
+                        /bin/chmod,\\
+                        /bin/mv" >> /etc/sudoers
 
-sudo echo "
-%alunos ALL=NOPASSWD: \
-       	/usr/bin/docker exec -it * login" >> /etc/sudoers
+sudo echo "%alunos		ALL=NOPASSWD:\\
+			/usr/bin/docker exec -it * login" >> /etc/sudoers
 
 cd ../users
 sudo chmod 764 add.sh
