5f0738b85187088d9c99e21439cc45e65b7f5be8
Walafi02 <walafif@yahoo.com>
2018-05-05 09:41:45 -0300

    correção de erro em confFinais

--- arquivos/confFinais.sh
+++ arquivos/confFinais.sh
@@ -5,6 +5,9 @@ sudo echo "www-data        ALL=NOPASSWD: ALL" >> /etc/sudoers
 sudo echo "%alunos		ALL=NOPASSWD:\\
 			/usr/bin/docker exec -it * login" >> /etc/sudoers
 
+mkdir ../executar
+mkdir ../matriculas
+
 cd ../users
 sudo chmod 764 add.sh
 
